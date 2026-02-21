5119cbb941cbb349c7587c42b2a2731b09aa82a4
Alexandru Coderie <alexcoderie5@gmail.com>
2026-02-21 18:39:56 +0200

    Refactored LuaSnip config

--- lua/coderiu/lazy/snippets.lua
+++ lua/coderiu/lazy/snippets.lua
@@ -3,12 +3,12 @@ return {
         "L3MON4D3/LuaSnip",
         version = "v2.*",
         build = "make install_jsregexp",
-        dependencies = { "rafamadriz/friendly-snippets" },
-        config = function()
-            local luasnip = require 'luasnip'
-            luasnip.config.setup {}
-            require('luasnip.loaders.from_vscode').lazy_load()
-        end
-
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+            config = function()
+                require('luasnip.loaders.from_vscode').lazy_load()
+            end
+        },
+        opts = {},
     }
 }
