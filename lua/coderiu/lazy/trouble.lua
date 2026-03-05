29f71c028e501bd27b479d7bfe469f7a9da3c81e
Alexandru Coderie <alexcoderie5@gmail.com>
2026-03-05 21:32:41 +0200

    Added trouble.nvim alongside a basic configuration

---
+++ lua/coderiu/lazy/trouble.lua
@@ -0,0 +1,19 @@
+return {
+    "folke/trouble.nvim",
+
+    config = function()
+        require("trouble").setup({})
+
+        vim.keymap.set("n", "<leader>tt", function ()
+            require("trouble").toggle({mode = "diagnostics"});
+        end, {desc = '[T]oggle [T]rouble'})
+
+        vim.keymap.set("n", "]t", function ()
+            require("trouble").next({skip_groups = true, jump = true});
+        end)
+
+        vim.keymap.set("n", "[t", function ()
+            require("trouble").prev({skip_groups = true, jump = true});
+        end)
+    end
+}
