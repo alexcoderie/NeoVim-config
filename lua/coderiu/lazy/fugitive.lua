ea8480752da15a17d03771df36d6a773a03db6ba
Alexandru Coderie <alexcoderie5@gmail.com>
2026-03-11 21:09:40 +0200

    Added shortcuts for git diffs

--- lua/coderiu/lazy/fugitive.lua
+++ lua/coderiu/lazy/fugitive.lua
@@ -2,5 +2,7 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+        vim.keymap.set('n', 'ga', '<cmd>diffget //2<CR>')
+        vim.keymap.set('n', 'g;', '<cmd>diffget //3<CR>')
     end
 }
